8ae9aaaed02cfc3c34aeda68b93cdb7a23aed8a0
Antônio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-06-09 03:47:40 -0300

    Validations and gitignore

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 
 import PointsController from './controllers/PointsController';
@@ -16,7 +17,24 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 //Criar um ponto de coleta
-routes.post('/points', upload.single('image'), pointsController.create); 
+routes.post(
+  '/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  },{
+    abortEarly: false
+  }),
+  pointsController.create); 
 routes.get('/points', pointsController.index); 
 //Listar ponto de forma individual
 routes.get('/points/:id', pointsController.show);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 
 const app = express();
@@ -10,9 +11,11 @@ app.use(express.json());
 app.use(routes);
 
 
+
+
 //função para puxar de forma estática elementos de um diretório específico
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-
+app.use(errors());
 
 app.listen(3333);
